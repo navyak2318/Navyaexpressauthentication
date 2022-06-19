7cd0a79a87eb76aae77788f315b36187f02e12db
navyak2318 <105392297+navyak2318@users.noreply.github.com>
2022-06-19 11:16:41 -0400

    edited

--- .history/server/config/db_20220619105146.js
+++
@@ -1,6 +0,0 @@
-module.exports = 
-{
-    //"URI": "mongodb://localhost/book_store"
-    "URI": "mongodb+srv://navyak2318:zoe50zxhMtV10iGl@cluster0.cey27x6.mongodb.net/book_store?retryWrites=true&w=majority",
-    "Secret": 'SomeSecret'
-}
